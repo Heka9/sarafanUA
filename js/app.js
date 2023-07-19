64f2e10a4c33bf92a16615d00d5b8bc73ea3fdc7
Yevhen Blyzniuk <90552593+Heka9@users.noreply.github.com>
2023-07-19 17:35:55 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4248,25 +4248,9 @@
         if (inputValue.trim() === "") e.preventDefault();
     }
     const buttonBack = document.querySelector(".menu-footer__link");
-    let firstLoad = sessionStorage.getItem("backUrl");
-    if (buttonBack && !firstLoad) {
-        buttonBack.href = "javascript:void(0)";
-        const currentUrl = window.location.href;
-        sessionStorage.setItem("backUrl", currentUrl);
-        firstLoad = false;
-    } else if (!firstLoad) {
-        const currentUrl = window.location.href;
-        sessionStorage.setItem("backUrl", currentUrl);
-        firstLoad = false;
-    } else if (buttonBack && firstLoad) {
-        const currentUrl = window.location.href;
-        const backUrl = sessionStorage.getItem("backUrl");
-        console.log(backUrl);
-        sessionStorage.setItem("backUrl", currentUrl);
-        buttonBack.href = backUrl;
-    } else if (firstLoad) {
-        const currentUrl = window.location.href;
-        sessionStorage.setItem("backUrl", currentUrl);
+    if (buttonBack) buttonBack.addEventListener("click", goBack);
+    function goBack() {
+        window.history.back();
     }
     window.addEventListener("DOMContentLoaded", windowLoad);
     let pageLoadingCounter = 1;
